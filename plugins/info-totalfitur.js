b709bb9bb546f392bb01bec6a0e5a5d050aae50a
Vannzzbit <111992857+Vannzzbit@users.noreply.github.com>
2023-01-25 20:31:27 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -9,7 +9,7 @@ contextInfo: { externalAdReply :{
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'ꜰʀᴏɴʙᴏᴛ',          previewType: 0,
+                        body: 'ᴠᴀɴᴢʙᴏᴛ',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: sgh
                       }}
