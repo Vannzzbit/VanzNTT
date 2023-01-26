f825d6d49e09cbb21d43764ed053a2454024b2c1
Vannzzbit <111992857+Vannzzbit@users.noreply.github.com>
2023-01-26 15:08:05 +0700

    Update info-ytbot.js

--- plugins/info-ytbot.js
+++ plugins/info-ytbot.js
@@ -3,15 +3,15 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `*SUBS YT XYNOZ*\n\nhttps://youtube.com/channel/UCm2jNvB02_cejKbLpN574hQ\n\n Jangan Lupa Subscribe Untuk Mengetahui Video Terbaru..`,wm + '\n\n' + botdate, giflogo, [['OK','ok']], m, {
+ await conn.sendButton(m.chat, `*Follow TT Owner*\n\ntiktok.com/@kelasdekk98\n\n Jangan Lupa Subscribe Untuk Mengetahui Video Terbaru..`,wm + '\n\n' + botdate, giflogo, [['OK','ok']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: '⚘ ᴄʀᴇᴀᴛᴇ ʙʏ xʏɴᴏᴢ',          previewType: 0,
+                        body: '⚘ ᴄʀᴇᴀᴛᴇ ʙʏ Vanz',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: 'https://youtube.com/channel/UCpn5v0YkmK8jJxVWETYU02Q'
+                        sourceUrl: 'tiktok.com/@kelasdekk98'
                       }}
 })
 }
