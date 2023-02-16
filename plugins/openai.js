06ac50b8164e0b618a88730c24dcc2d5efcc8de2
Vannzzbit <elvanoadepratama@gmail.com>
2023-02-16 18:46:50 +0700

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -1,29 +1,12 @@
-/*
-import fetch from 'node-fetch'
-let handler = async (m, {
- text, 
- usedPrefix, 
- command
- }) => {
-    if (!text) throw `Contoh:\n${usedPrefix + command} Apa itu OpenAI`
-let res = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${global.lolkey}&text=${text}&user=user-unique-id`)
-if (!res.ok) throw eror
-        let json = await res.json()
-        await m.reply(`${json.result}`)
-        return !0
-}
-handler.help = ['ai <pertanyaan>']
-handler.tags = ['main']
-handler.command = /^(ai)$/i
-handler.limit = false
-handler.register = true
-export default handler
-*/
 import { Configuration, OpenAIApi } from "openai";
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `Contoh:\n${usedPrefix + command} Apa itu OpenAI`
+import fetch from "node-fetch"
+import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+
+import fs from 'fs'
+let handler = async (m, { conn, text }) => {
+if (!text) throw "Hay adakah yang bisa saya bantu??"
 const configuration = new Configuration({
-    apiKey: "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" //api key bisa didapatkan dari https://openai.com/api/
+    apiKey: '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'
 });
 const openai = new OpenAIApi(configuration);
         const response = await openai.createCompletion({
@@ -35,12 +18,9 @@ const openai = new OpenAIApi(configuration);
             frequency_penalty: 0.5,
             presence_penalty: 0
         });
-            m.reply(response.data.choices[0].text)
+m.reply(response.data.choices[0].text)
     }
-handler.help = ['ai <pertanyaan>']
-handler.tags = ['ai']
-handler.command = /^(ai)$/i
-handler.limit = false
-handler.register = true
-
+handler.help = ['openai']
+handler.tags = ['fun']
+ handler.command = /^(yus|ai|openai)$/i
 export default handler
