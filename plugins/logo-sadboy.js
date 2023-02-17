148ac5b5eaaf0820a4c975cfdd921e9969731d1f
Vannzzbit <elvanoadepratama@gmail.com>
2023-02-17 14:49:31 +0700

    Update logo-sadboy.js

--- plugins/logo-sadboy.js
+++ plugins/logo-sadboy.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
   m.reply('proses..')
-  let res = `https://ziy.herokuapp.com/api/maker/sadboy?text1=${response[0]}&text2=${response[1]}&apikey=xZiyy`
-  conn.sendFile(m.chat, res, 'sadboy.jpg', `Nih kak`, m, false)
+  let res = `https://restapi.frteam.xyz/sadboy?nama=${response[0]}&nama2=${response[1]}&apikey=085759681568`
+  conn.sendFile(m.chat, res, 'gfx3.jpg', `Nih kak`, m, false)
 }
 handler.help = ['sadboylogo'].map(v => v + ' <text|text>')
-handler.tags = ['logo']
+handler.tags = ['maker']
 handler.command = /^(logosadboy)$/i
-handler.limit = false
+handler.limit = true
 
 export default handler
