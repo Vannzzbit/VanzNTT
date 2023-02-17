c91dfdfd0a6fc29707819281b23153d0a99f8d20
Vannzzbit <elvanoadepratama@gmail.com>
2023-02-17 14:48:53 +0700

    Update logo-neko.js

--- plugins/logo-neko.js
+++ plugins/logo-neko.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
   m.reply('Proses...')
-  let res = `https://ziy.herokuapp.com/api/maker/girlneko?text1=${response[0]}&text2=${response[1]}&apikey=xZiyy`
+  let res = `https://restapi.frteam.xyz/girlneko?nama=${response[0]}&nama2=${response[1]}&apikey=085759681568`
   conn.sendFile(m.chat, res, 'neko.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logoneko'].map(v => v + ' <text|text>')
-handler.tags = ['logo']
+handler.tags = ['maker']
 handler.command = /^(logoneko)$/i
-handler.limit = false
+handler.limit = true
 
 export default handler
