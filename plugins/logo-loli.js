9828d037dc0d7f4d1cc39cec46f49a43eba025e1
Vannzzbit <elvanoadepratama@gmail.com>
2023-02-17 14:53:12 +0700

    Update logo-loli.js

--- plugins/logo-loli.js
+++ plugins/logo-loli.js
@@ -1,16 +1,16 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
   m.reply('Proses...')
-  let res = `https://ziy.herokuapp.com/api/maker/lolimaker?nama=${response[0]}&apikey=xZiyy`
+  let res = `https://restapi.frteam.xyz/lolimaker?nama=${response[0]}&apikey=085759681568`
   conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text>')
-handler.tags = ['logo']
+handler.tags = ['maker']
 handler.command = /^(logololi)$/i
-handler.register = false
+handler.register = true
 
-handler.limit = false
+handler.limit = true
 
 export default handler
